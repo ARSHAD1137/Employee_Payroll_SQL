67d0e108ac533bdc54bc993f023fac5411877630
Arshad <arshadpinjari666@gmail.com>
2021-06-04 18:40:25 +0530

    Added:UC7

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -37,3 +37,20 @@ update employee_payroll set gender='M' where name in ('Ramesh','Jack')
 
 select * from employee_payroll
 
+select sum(salary) from employee_payroll where gender='M' group by gender;
+
+select sum(salary) from employee_payroll where gender='F' group by gender;
+
+select avg(salary) from employee_payroll where gender = 'M' group by gender;
+
+select min(salary) from employee_payroll where gender = 'M' group by gender;
+
+select max(salary) from employee_payroll where gender = 'M' group by gender;
+
+select count(salary) from employee_payroll where gender = 'M' group by gender;
+
+select count(salary) from employee_payroll where gender = 'F' group by gender;
+
+
+
+
